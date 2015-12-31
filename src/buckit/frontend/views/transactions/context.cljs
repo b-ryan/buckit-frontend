fbe9daabbb2f06abb7e1af57a2cd7e20611a0481
Buck Ryan <buck.ryan@gmail.com>
2015-12-30 19:04:58 -0500

    move more functionality into context namespace, fix more issues showing every transaction

--- src/buckit/frontend/views/transactions/context.cljs
+++ src/buckit/frontend/views/transactions/context.cljs
@@ -8,7 +8,12 @@
                            (optional -- default: nil)
 
   :edit?                   Indicaes whether edit mode will be used.
-                           (optional -- default: false)") 
+                           (optional -- default: false)"
+  (:require [buckit.frontend.models.core        :as models]
+            [buckit.frontend.models.split       :as models.split]
+            [buckit.frontend.models.transaction :as models.transaction]))
+
+(def valid-mode? (partial contains? #{:no-account :single-account :multi-account}))
 
 (defn mode
   [context]
@@ -21,3 +26,58 @@
 (defn is-selected?
   [context transaction-id]
   (= (:selected-transaction-id context) transaction-id))
+
+; ----------------------------------------------------------------------------
+(defmulti transactions-query mode)
+
+(defmethod transactions-query :no-account
+  [_]
+  {:query-id [:load-transactions]
+   :method   :get-many
+   :resource models/transactions})
+
+(defmethod transactions-query :single-account
+  [{:keys [account-id]}]
+  {:query-id [:load-transactions account-id]
+   :method   :get-many
+   :resource models/transactions
+   :args     [{:filters [{:name "splits__account_id"
+                          :op   "any"
+                          :val  account-id}]}]})
+
+; ----------------------------------------------------------------------------
+(defmulti filter-transactions mode)
+
+(defmethod filter-transactions :no-account
+  [_ transactions]
+  transactions)
+
+(defmethod filter-transactions :single-account
+  [{:keys [account-id]} transactions]
+  (models/account-transactions account-id transactions))
+
+; ----------------------------------------------------------------------------
+(defmulti main-split mode)
+
+(defmethod main-split :single-account
+  [context transaction]
+  (models.split/split-for-account
+    (models.transaction/splits transaction)
+    (:account-id context)))
+
+(defmethod main-split :default
+  [_ transaction]
+  (first (models.transaction/splits transaction)))
+
+; ----------------------------------------------------------------------------
+(defmulti other-splits mode)
+
+(defmethod other-splits :single-account
+  [context transaction]
+  (models.split/splits-for-other-accounts
+    (models.transaction/splits transaction)
+    (:account-id context)))
+
+(defmethod other-splits :default
+  [_ transaction]
+  (rest (models.transaction/splits transaction)))
--- src/buckit/frontend/views/transactions/ledger.cljs
+++ src/buckit/frontend/views/transactions/ledger.cljs
@@ -13,37 +13,6 @@
             [buckit.frontend.views.transactions.events  :as events]
             [re-frame.core                              :refer [dispatch subscribe]]))
 
-
-
-(defmulti ^:private transactions-query ctx/mode)
-
-(defmethod ^:private transactions-query :no-account
-  [_]
-  {:query-id [:load-transactions]
-   :method   :get-many
-   :resource models/transactions})
-
-(defmethod ^:private transactions-query :single-account
-  [{:keys [account-id]}]
-  {:query-id [:load-transactions account-id]
-   :method   :get-many
-   :resource models/transactions
-   :args     [{:filters [{:name "splits__account_id"
-                          :op   "any"
-                          :val  account-id}]}]})
-
-
-(defmulti ^:private filter-transactions ctx/mode)
-
-(defmethod ^:private filter-transactions :no-account
-  [_ transactions]
-  transactions)
-
-(defmethod ^:private filter-transactions :single-account
-  [{:keys [account-id]} transactions]
-  (models/account-transactions account-id transactions))
-
-
 (def ledger-header
   [:div.container-fluid
    [:div.row.buckit--ledger-header
@@ -53,17 +22,16 @@
     [:span.col-sm-3.hidden-xs "Memo"]
     [:span.col-sm-2.col-xs-4 "Amount"]]])
 
-
-
 (defn- account-to-show
-  [accounts other-splits]
-  (if (> (count other-splits) 1)
-    "Splits"
-    (->> other-splits
-         first
-         models.split/account-id
-         (get accounts)
-         models.account/name)))
+  [context transaction accounts] ; FIXME make accounts part of context?
+  (let [other-splits (ctx/other-splits context transaction)]
+    (if (> (count other-splits) 1)
+      "Splits"
+      (->> other-splits
+           first
+           models.split/account-id
+           (get accounts)
+           models.account/name))))
 
 (defn- amount-glyphicon
   "Returns a left or right arrow glyphicon to show whether money left the
@@ -76,9 +44,8 @@
          (if expense? "expense" "income"))))
 
 (defn- amount-to-show
-  [main-split]
-  {:pre [(some? main-split)]}
-  (let [amount   (:amount main-split)]
+  [context transaction]
+  (let [amount (:amount (ctx/main-split context transaction))]
     [:span {:class (amount-glyphicon amount)
             :aria-hidden true}
      ; FIXME other currencies?
@@ -89,21 +56,17 @@
   (let [accounts (subscribe [:accounts])
         payees   (subscribe [:payees])]
     (fn
-      [{:keys [account-id] :as context} transaction]
-      (let [splits       (:splits transaction)
-            main-split   (models.split/split-for-account splits account-id)
-            other-splits (models.split/splits-for-other-accounts splits account-id)]
-        (assert main-split)
-        [:div.row
-         {:on-click (events/transaction-clicked-fn context transaction)}
-         [:span.col-sm-2.col-xs-4 (:date transaction)]
-         [:span.col-sm-2.hidden-xs (->> transaction
-                                        models.transaction/payee-id
-                                        (get @payees)
-                                        models.payee/name)]
-         [:span.col-sm-3.col-xs-4 (account-to-show @accounts other-splits)]
-         [:span.col-sm-3.hidden-xs]
-         [:span.col-sm-2.col-xs-4 (amount-to-show main-split)]]))))
+      [context transaction]
+      [:div.row
+       {:on-click (events/transaction-clicked-fn context transaction)}
+       [:span.col-sm-2.col-xs-4 (:date transaction)]
+       [:span.col-sm-2.hidden-xs (->> transaction
+                                      models.transaction/payee-id
+                                      (get @payees)
+                                      models.payee/name)]
+       [:span.col-sm-3.col-xs-4 (account-to-show context transaction @accounts)]
+       [:span.col-sm-3.hidden-xs]
+       [:span.col-sm-2.col-xs-4 (amount-to-show context transaction)]])))
 
 (defn- ledger-row
   [context transaction]
@@ -125,9 +88,9 @@
       [{:keys [account-id selected-transaction-id] :as context}]
       {:pre [(utils/nil-or-integer? account-id)
              (utils/nil-or-integer? selected-transaction-id)]}
-      (let [query            (transactions-query context)
+      (let [query            (ctx/transactions-query context)
             query-result     (get @queries (:query-id query))
-            transactions     (filter-transactions context (vals @transactions))]
+            transactions     (ctx/filter-transactions context (vals @transactions))]
         (cond
           ; -----------------------------------------------------------------
           ; NO QUERY
